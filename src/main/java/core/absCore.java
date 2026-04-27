3d8836dcc7bca1c750a2145f2a180fc5a1ff06d6
Alexandre <alexandrevps22@proton.me>
2026-04-27 17:58:57 -0300

    /

--- src/main/java/core/absCore.java
+++ src/main/java/core/absCore.java
@@ -1,7 +1,18 @@
 package core;
 
 
-public class CoreReader {
-    private final GetPath gpath=new GetPath();
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public abstract class absCore implements AutoCloseable{
+    protected Path path;
+    public absCore(String pth){
+        this.path=Paths.get(pth);
+
+    }
+
+    public abstract void openFile(File pth);
+    public abstract void deleteFile();
 
 }
