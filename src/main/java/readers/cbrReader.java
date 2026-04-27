fb2fb4a09046ecf51e48a9da0b147728ff1b6ce5
Alexandre <alexandrevps22@proton.me>
2026-04-27 12:39:53 -0300

    feat: new class for readers

---
+++ src/main/java/readers/cbrReader.java
@@ -0,0 +1,24 @@
+package readers;
+
+import core.absCore;
+
+public class cbrReader extends absCore {
+    public cbrReader(String pth) {
+        super(pth);
+    }
+
+    @Override
+    public void openFile() {
+
+    }
+
+    @Override
+    public void deleteFile() {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
