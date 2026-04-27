11632c6e1f0df31095625e94b22306bed7069c49
Alexandre <alexandrevps22@proton.me>
2026-04-27 13:57:51 -0300

    Handling the added file

--- src/main/java/readers/cbrReader.java
+++ src/main/java/readers/cbrReader.java
@@ -1,5 +1,11 @@
 package readers;
+import com.github.junrar.Archive;
+import com.github.junrar.exception.RarException;
+import com.github.junrar.rarfile.FileHeader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
 import core.absCore;
 
 public class cbrReader extends absCore {
@@ -8,7 +14,20 @@ public class cbrReader extends absCore {
     }
 
     @Override
-    public void openFile() {
+    public void openFile(File pth) {
+        try(Archive archive= new Archive(pth)) {
+            FileHeader header;
+            while ((header=archive.nextFileHeader())!=null){
+                if (!header.isDirectory()){
+                    InputStream is=archive.getInputStream(header);
+
+                }
+            }
+        } catch (RarException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
