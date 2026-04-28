e6192b7247f69d85fa7d8bd9116482b5dc653bb5
Alexandre <alexandrevps22@proton.me>
2026-04-27 23:25:32 -0300

    feat: Add a list to sort pages.

--- src/main/java/readers/cbrReader.java
+++ src/main/java/readers/cbrReader.java
@@ -1,5 +1,7 @@
 package readers;
 import com.github.junrar.Archive;
+import com.github.junrar.ContentDescription;
+import com.github.junrar.Junrar;
 import com.github.junrar.exception.RarException;
 import com.github.junrar.rarfile.FileHeader;
 
@@ -9,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.File;
 import core.absCore;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -21,10 +24,12 @@ public class cbrReader extends absCore {
     public void openFile(File pth) {
         try(Archive archive= new Archive(pth)) {
             FileHeader header;
+            final List<ContentDescription> contentFile= Junrar.getContentsDescription(pth);
             while ((header=archive.nextFileHeader())!=null){
                 if (!header.isDirectory()){
                     InputStream is=archive.getInputStream(header);
                     BufferedImage img=null;
+                    is.close();
                     try{
                         img=ImageIO.read(is);
                     } catch (IOException e) {
